de3ba5fc865ff5292f2a9c917f595d48677182dd
dhafinn93 <minnoru55@gmail.com>
2026-03-17 10:46:48 +0700

    input data

---
+++ latihan/python4.py
@@ -0,0 +1,11 @@
+# Input Data
+
+data= input("Masukkan data: ")
+print("data = ",data,",type = ",type(data))
+
+angka = float(input("Masukkan angka: "))
+angka = int(input("Masukkan angka: "))
+print("angka = ",angka,",type = ",type(angka))
+
+biner = bool(int(input("masukkan nilai : ")))
+print("data = ", biner,",type = ",type(biner))
