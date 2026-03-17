db87e99026b205d0f6068347b0c0b7630ce33b54
dhafinn93 <minnoru55@gmail.com>
2026-03-17 09:35:02 +0700

    first commit

---
+++ latihan/python1.py
@@ -0,0 +1,11 @@
+# variabel adalah tempat menyimpan data
+
+#assigment nilai
+a = 10
+b = 5
+panjang = 100
+
+print("Nilai a = ", a)
+print("Nilai b = ", b)
+print("Nilai panjang = ", panjang)
+
---
+++ latihan/python2.py
@@ -0,0 +1,9 @@
+#tipe data: Angka satuan yang gak ada koma(integer)
+data_integer = 1
+print("data : ", data_integer)
+print("-bertipe : ", type(data_integer))
+
+#tipe data: angka dengan koma(float)
+data_float= 1.5
+print("data : ", data_float)
+print("-bertipe : ", type(data_float))
